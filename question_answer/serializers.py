fa913beb190d796eed6d0879768ffb713c413f11
Julia Bengert <yubengert@gmail.com>
2025-08-29 18:12:24 +0300

    add views for Question, Answer

---
+++ question_answer/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from .models import Question, Answer
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Answer
+        fields = "__all__"
+
+
+class CreateAnswerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Answer
+        fields = ["question_id", "text"]
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Question
+        fields = "__all__"
+
+
+class RetrieveQuestionSerializer(serializers.ModelSerializer):
+
+    answers = AnswerSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Question
+        fields = "__all__"
+
+
+class CreateQuestionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Question
+        fields = ["text"]
--- question_answer/views.py
+++ question_answer/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Question, Answer
+from .serializers import QuestionSerializer, CreateQuestionSerializer, RetrieveQuestionSerializer, AnswerSerializer, CreateAnswerSerializer
 
-# Create your views here.
+
+class AnswerRetrieveAPIView(generics.RetrieveAPIView):
+
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+    permission_classes = []
+
+
+class AnswerCreateAPIView(generics.CreateAPIView):
+
+    queryset = Answer.objects.all()
+    serializer_class = CreateAnswerSerializer
+    permission_classes = []
+
+
+class AnswerDestroyAPIView(generics.DestroyAPIView):
+
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+    permission_classes = []
+
+
+class QuestionListAPIView(generics.ListAPIView):
+
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+    permission_classes = []
+
+
+class QuestionRetrieveAPIView(generics.RetrieveAPIView):
+
+    queryset = Question.objects.all()
+    serializer_class = RetrieveQuestionSerializer
+    permission_classes = []
+
+
+class QuestionCreateAPIView(generics.CreateAPIView):
+
+    queryset = Question.objects.all()
+    serializer_class = CreateQuestionSerializer
+    permission_classes = []
+
+
+class QuestionDestroyAPIView(generics.DestroyAPIView):
+
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+    permission_classes = []
