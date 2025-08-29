acbd50728103e1ec4899c006cc7f8569ba597e12
Julia Bengert <yubengert@gmail.com>
2025-08-29 18:26:27 +0300

    add urls for views in question_answer

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("forum/", include("question_answer.urls", namespace="question_answer")),
+    path("forum/", include("question_answer.urls", namespace="question_answer")),
     # path("user/", include("users.urls", namespace="user")),
 ]
--- question_answer/urls.py
+++ question_answer/urls.py
@@ -1,10 +1,18 @@
-# from django.urls import path
-# from .apps import QuestionAnswerConfig
-#
-#
-# app_name = QuestionAnswerConfig.name
-#
-#
-# urlpatterns = [
-#     path(),
-# ]
+from django.urls import path
+from .apps import QuestionAnswerConfig
+from . import views
+
+
+app_name = QuestionAnswerConfig.name
+
+
+urlpatterns = [
+    path("questions/", views.QuestionListAPIView.as_view(), name="question_list"),
+    path("question/<int:pk>/", views.QuestionRetrieveAPIView.as_view(), name="question_retrieve"),
+    path("question/new/", views.QuestionCreateAPIView.as_view(), name="question_create"),
+    path("question/<int:pk>/", views.QuestionDestroyAPIView.as_view(), name="question_delete"),
+
+    path("answer/<int:pk>/", views.AnswerRetrieveAPIView.as_view(), name="answer_retrieve"),
+    path("answer/new/", views.AnswerCreateAPIView.as_view(), name="answer_create"),
+    path("answer/<int:pk>/delete/", views.AnswerDestroyAPIView.as_view(), name="answer_delete"),
+]
