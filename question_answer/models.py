30ef1c41cfc2708bcd2ca39fc77f7c3f3be38a45
Julia Bengert <yubengert@gmail.com>
2025-08-29 16:38:13 +0300

    add user_id field in Answer model

--- question_answer/models.py
+++ question_answer/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from users.models import User
 
 
 class Question(models.Model):
+
     text = models.TextField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Answer(models.Model):
+
     question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.SET(0))
     text = models.TextField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
