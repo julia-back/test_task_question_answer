009cf3cd7a97dd5b4b418f3b56936c9d04d8d527
Julia Bengert <yubengert@gmail.com>
2025-08-29 16:50:15 +0300

    add namespace for urls

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # path("forum/", include("question_answer.urls", namespace="question_answer")),
+    # path("user/", include("users.urls", namespace="user")),
 ]
---
+++ question_answer/urls.py
@@ -0,0 +1,10 @@
+# from django.urls import path
+# from .apps import QuestionAnswerConfig
+#
+#
+# app_name = QuestionAnswerConfig.name
+#
+#
+# urlpatterns = [
+#     path(),
+# ]
