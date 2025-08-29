bb5f59760c755d7a7bb8e5b26658b2dbb449ca36
Julia Bengert <yubengert@gmail.com>
2025-08-29 16:02:13 +0300

    add models Question and Answer

--- question_answer/models.py
+++ question_answer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    text = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Answer(models.Model):
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
